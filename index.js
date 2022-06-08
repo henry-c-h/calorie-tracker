04ffdef491d8e005389aa976c77c095d4e8c41a8
henrychen <henrychen91@outlook.com>
2022-06-08 21:06:08 +0100

    update index.js

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
 app.use('/api/diary', diaryRoute);
 app.use('/api/goal', goalRoute);
 app.use('/api/auth', authRoute);
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 app.listen(PORT, () => console.log(`server running at ${PORT}`));
