bea5a258f89317a599ced99d5c83fb6d26c88806
henrychen <henrychen91@outlook.com>
2022-06-08 21:01:19 +0100

    update index.js

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 app.use('/api/diary', diaryRoute);
 app.use('/api/goal', goalRoute);
